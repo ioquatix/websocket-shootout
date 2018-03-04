114c5a415830c61ca3155d02b7ccbf0f774265a2
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-03-04 19:59:02 +0300

    Fix anycable script

--- elixir/phoenix_socket/config/dev.exs
+++ elixir/phoenix_socket/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :phoenix_socket, PhoenixSocket.Endpoint,
-  http: [port: 4000],
+  http: [port: 3334, ip: {0, 0, 0, 0}],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
--- ruby/action-cable-server/config/environment.rb
+++ ruby/action-cable-server/config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+require 'anycable-rails' if ENV['USE_ANYCABLE']
 require_relative '../lib/actioncable_patch' if ENV['PATCH']
 # Initialize the Rails application.
 Rails.application.initialize!
